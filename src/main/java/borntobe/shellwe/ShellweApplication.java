9af49c085265aef3905cf64e89770abd866cb6e4
Kimmoongi0320 <moongi0320@naver.com>
2025-07-15 18:15:58 +0900

    Initial commit

---
+++ src/main/java/borntobe/shellwe/ShellweApplication.java
@@ -0,0 +1,13 @@
+package borntobe.shellwe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShellweApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShellweApplication.class, args);
+	}
+
+}
---
+++ src/test/java/borntobe/shellwe/ShellweApplicationTests.java
@@ -0,0 +1,13 @@
+package borntobe.shellwe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShellweApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
