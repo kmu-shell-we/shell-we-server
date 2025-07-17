62ec5134e21bc57a2b199510f4ffd7d7ea26276b
Son Daehyeon <sondaehyeon01@gmail.com>
2025-07-17 15:23:23 +0900

    [SW-10] chore(init): 스프링 부트 초기 설정 (#1)
    Co-authored-by: BORB <qzsec299@naver.com>

---
+++ src/main/java/com/github/kmu_shell_we/ShellWeApplication.java
@@ -0,0 +1,13 @@
+package com.github.kmu_shell_we;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShellWeApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ShellWeApplication.class, args);
+    }
+}
