3ed3747df5dcc7131d682b75469d69eef53bd312
arirang030 <arirang030@kookmin.ac.kr>
2025-07-20 19:31:20 +0900

    [SW-12] fix: 코드 리뷰 후 수정

--- src/main/java/com/github/kmu_shell_we/domain/user/schema/User.java
+++ src/main/java/com/github/kmu_shell_we/domain/user/schema/User.java
@@ -5,16 +5,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @EqualsAndHashCode(callSuper = true)
 public class User extends BaseSchema {
@@ -44,34 +40,8 @@ public class User extends BaseSchema {
 
     public enum Role {
         MEMBER,
-        ADMIN(MEMBER), // ADMIN은 MEMBER의 권한을 상속받음
+        ADMIN, // ADMIN은 MEMBER의 권한을 상속받음
         ;
-
-        // 상속된 역할들
-        private final Role[] inheritedRoles;
-
-        // 생성자
-        Role(Role... inheritedRoles) {
-            this.inheritedRoles = inheritedRoles;
-        }
-
-        // 권한 리스트 반환
-        public Collection<SimpleGrantedAuthority> getAuthorities() {
-            Set<Role> authorization = new HashSet<>();
-            collectionAuthorization(this, authorization);
-            return authorization.stream()
-                    .map(role -> "ROLE_" + role.name())
-                    .map(SimpleGrantedAuthority::new)
-                    .toList();
-        }
-
-        // 재귀적으로 상속된 역할 모두 수집
-        private void collectionAuthorization(Role role, Set<Role> roles) {
-            roles.add(role); // 자기 자신을 추가..
-            for (Role inheritedRole : role.inheritedRoles) {
-                collectionAuthorization(inheritedRole, roles); // 상속된 역할도 재귀적으로 수집
-            }
-        }
     }
 
 }
