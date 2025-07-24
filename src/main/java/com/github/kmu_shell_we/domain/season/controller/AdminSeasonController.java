6cbb037a472c93a5ff0ce0dbf9d8fcedc6cdff0c
arirang030 <arirang030@kookmin.ac.kr>
2025-07-24 20:54:37 +0900

    [SW-39] refactor(season): 주석 처리

--- src/main/java/com/github/kmu_shell_we/domain/season/controller/AdminSeasonController.java
+++ src/main/java/com/github/kmu_shell_we/domain/season/controller/AdminSeasonController.java
@@ -6,12 +6,13 @@ import com.github.kmu_shell_we.domain.season.service.SeasonService;
 import com.github.kmu_shell_we.global.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@PreAuthorize("hasRole('ADMIN')")
+@PreAuthorize("hasRole('ADMIN')")
 @RestController
 @RequestMapping("/admin/seasons")
 @RequiredArgsConstructor
@@ -21,9 +22,7 @@ public class AdminSeasonController {
 
     @PostMapping
     public ApiResponse<SeasonResponse> createSeason(@RequestBody @Valid CreateSeasonRequest request) {
-        // 시즌 생성 <파이프라인>
-        // 사용자 -> requestBody -> CreateSeasonRequest dto -> here
-        // Season entity 생성 -> Repository 저장 -> 성공적으로 저장되었다면 SeasonResponse
+
         return ApiResponse.ok(seasonService.createSeason(request));
     }
 }
