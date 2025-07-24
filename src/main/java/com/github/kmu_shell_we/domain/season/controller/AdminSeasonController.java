8e567e73d2b29d8ddf57fe743b74b795e269e5bb
arirang030 <arirang030@kookmin.ac.kr>
2025-07-24 21:30:22 +0900

    [SW-40] feat(season): 시즌 수정 서비스 구현

--- src/main/java/com/github/kmu_shell_we/domain/season/controller/AdminSeasonController.java
+++ src/main/java/com/github/kmu_shell_we/domain/season/controller/AdminSeasonController.java
@@ -1,16 +1,16 @@
 package com.github.kmu_shell_we.domain.season.controller;
 
 import com.github.kmu_shell_we.domain.season.dto.request.CreateSeasonRequest;
+import com.github.kmu_shell_we.domain.season.dto.request.UpdateSeasonRequest;
 import com.github.kmu_shell_we.domain.season.dto.response.SeasonResponse;
 import com.github.kmu_shell_we.domain.season.service.SeasonService;
 import com.github.kmu_shell_we.global.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @PreAuthorize("hasRole('ADMIN')")
 @RestController
@@ -24,5 +24,11 @@ public class AdminSeasonController {
     public ApiResponse<SeasonResponse> createSeason(@RequestBody @Valid CreateSeasonRequest request) {
 
         return ApiResponse.ok(seasonService.createSeason(request));
+    }
+
+    @PutMapping("/{seasonId}")
+    public ApiResponse<SeasonResponse> updateSeason(@PathVariable UUID seasonId, @RequestBody @Valid UpdateSeasonRequest request) {
+
+        return ApiResponse.ok(seasonService.updateSeason(seasonId, request));
     }
 }
