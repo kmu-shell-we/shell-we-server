f3e5dd47a5f59f7537dabfbd73afe0fc9379e54c
arirang030 <arirang030@kookmin.ac.kr>
2025-07-24 14:03:54 +0900

    [SW-14] feat(season): 엔티티 및 레포지토리

---
+++ src/main/java/com/github/kmu_shell_we/domain/season/repository/SeasonRepository.java
@@ -0,0 +1,10 @@
+package com.github.kmu_shell_we.domain.season.repository;
+
+import com.github.kmu_shell_we.domain.season.entity.Season;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface SeasonRepository extends JpaRepository<Season, UUID> {
+
+}
