d5b08ae39541e13367fa51a5e6788f1fed6a5f42
arirang030 <145207957+arirang030@users.noreply.github.com>
2025-07-28 18:34:17 +0900

    [0.3.0] SW-53 전역 예외 핸들러 구현 (#19)
    * [SW-53] feat(exception): 전역 예외 핸들러 구현
    
    * [SW-53] refactor(): 처리할 수 없는 기타 오류 구현 및 버전 수정

--- src/main/java/com/github/kmu_shell_we/global/exception/ApiExceptionHandler.java
+++ src/main/java/com/github/kmu_shell_we/global/exception/ApiExceptionHandler.java
@@ -2,16 +2,64 @@ package com.github.kmu_shell_we.global.exception;
 
 import com.github.kmu_shell_we.global.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authorization.AuthorizationDeniedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.util.Objects;
 
 @Slf4j
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
+    @ExceptionHandler({ NoResourceFoundException.class, HttpRequestMethodNotSupportedException.class})
+    public ApiResponse<?> noResourceFoundException(Exception e) {
+
+        return ApiResponse.error("GLOBAL_001", "요청하신 리소스를 찾을 수 없습니다.");
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ApiResponse<?> httpMessageNotReadableException(HttpMessageNotReadableException e) {
+
+        return ApiResponse.error("GLOBAL_002", "요청 데이터가 올바르지 않습니다.");
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ApiResponse<?> authorizationDeniedException(AuthorizationDeniedException e) {
+
+        return ApiResponse.error("GLOBAL_003", "권한이 없습니다.");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ApiResponse<?> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        if (Objects.isNull(e.getBindingResult().getFieldError())) {
+
+            return ApiResponse.error("GLOBAL_004", e.getMessage());
+        }
+
+        String field = e.getBindingResult().getFieldError().getField();
+        String message = e.getBindingResult().getFieldError().getDefaultMessage();
+        String errorMessage = String.format("%s은(는) %s",  field, message);
+
+        return ApiResponse.error("GLOBAL_005", errorMessage);
+    }
+
     @ExceptionHandler(ApiException.class)
     public ApiResponse<?> apiException(ApiException e) {
 
         return ApiResponse.error(e);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ApiResponse<?> exception(Exception e) {
+
+        log.error("", e);
+
+        return ApiResponse.error("GLOBAL_005", "알 수 없는 오류가 발생했습니다.");
     }
 }
