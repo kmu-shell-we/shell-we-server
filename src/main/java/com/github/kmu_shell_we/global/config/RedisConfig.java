f5ef9357dce1b1a03b65991b614871976c7445e9
arirang030 <145207957+arirang030@users.noreply.github.com>
2025-08-03 22:27:17 +0900

    [0.4.0] SW-62 Redis를 설정한다. (#25)
    * [SW-62] feat(redis): RedisConfig 설정
    
    * [SW-62] feat(gradle): 버전 수정
    
    * [SW-62] refactor(redis): config와 property 분리
    
    * [SW-62] refactor(redis): password 추가 및 환경 변수 설정
    
    * [SW-62] refactor(redis): password 관련 리뷰 수정

---
+++ src/main/java/com/github/kmu_shell_we/global/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.github.kmu_shell_we.global.config;
+
+import com.github.kmu_shell_we.global.property.RedisProperty;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    private final RedisProperty redisProperty;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisProperty.getHost(), redisProperty.getPort());
+
+        config.setPassword(redisProperty.getPassword());
+
+        return new LettuceConnectionFactory(config);
+    }
+}
