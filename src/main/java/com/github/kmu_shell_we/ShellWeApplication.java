f4d063782fdf90ff42965bc937fb7fd0d3c401fb
BORB <qzsec299@naver.com>
2025-08-07 04:55:52 +0900

    [SW-67] feat(teamMission): 일간, 주간 스케쥴링으로 팀 미션 랜덤 배정

--- src/main/java/com/github/kmu_shell_we/ShellWeApplication.java
+++ src/main/java/com/github/kmu_shell_we/ShellWeApplication.java
@@ -2,8 +2,10 @@ package com.github.kmu_shell_we;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ShellWeApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/github/kmu_shell_we/domain/season/_team/_mission_team/service/TeamMissionService.java
+++ src/main/java/com/github/kmu_shell_we/domain/season/_team/_mission_team/service/TeamMissionService.java
@@ -1,5 +1,8 @@
 package com.github.kmu_shell_we.domain.season._team._mission_team.service;
 
+import com.github.kmu_shell_we.domain.mission._season_mission.entity.SeasonMission;
+import com.github.kmu_shell_we.domain.mission._season_mission.repository.SeasonMissionRepository;
+import com.github.kmu_shell_we.domain.mission.constant.MissionType;
 import com.github.kmu_shell_we.domain.season._team._mission_team.dto.response.TeamMissionListResponse;
 import com.github.kmu_shell_we.domain.season._team._mission_team.entity.TeamMission;
 import com.github.kmu_shell_we.domain.season._team._mission_team.repository.TeamMissionRepository;
@@ -10,11 +13,17 @@ import com.github.kmu_shell_we.domain.season.entity.Season;
 import com.github.kmu_shell_we.domain.season.exception.SeasonExceptions;
 import com.github.kmu_shell_we.domain.season.repository.SeasonRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +32,7 @@ public class TeamMissionService {
     private final TeamMissionRepository teamMissionRepository;
     private final TeamRepository teamRepository;
     private final SeasonRepository seasonRepository;
+    private final SeasonMissionRepository seasonMissionRepository;
 
     @Transactional(readOnly = true)
     public TeamMissionListResponse getTeamMissions(UUID seasonId, UUID teamId) {
@@ -36,5 +46,57 @@ public class TeamMissionService {
         List<TeamMission> teamMissions = teamMissionRepository.findAllBySeasonAndTeam(season, team);
 
         return TeamMissionListResponse.from(teamMissions);
+    }
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    @Transactional
+    public void assignDailyMission() {
+
+        addRandomMission(MissionType.DAILY,
+                now -> now.toLocalDate().plusDays(1).atStartOfDay());
+    }
+
+    @Scheduled(cron = "0 0 0 ? * MON")
+    @Transactional
+    public void assignWeeklyMission() {
+
+        addRandomMission(MissionType.WEEKLY,
+                now -> now.toLocalDate()
+                        .with(TemporalAdjusters.next(DayOfWeek.SUNDAY))
+                        .plusDays(1)
+                        .atStartOfDay());
+    }
+
+    private void addRandomMission(
+            MissionType missionType,
+            Function<LocalDateTime, LocalDateTime> endCalculator
+    ) {
+
+        LocalDateTime now = LocalDateTime.now();
+
+        Season season = seasonRepository.findCurrentSeason();
+        if(season == null) return;
+
+        List<SeasonMission> missions = seasonMissionRepository
+                .findAllBySeasonAndMissionType(season, missionType);
+        if (missions.isEmpty()) return;
+
+        List<Team> teams = teamRepository.findAllBySeason(season);
+        if (teams.isEmpty()) return;
+
+        LocalDateTime endedAt = endCalculator.apply(now);
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+
+        for (Team team : teams) {
+            SeasonMission randomMission = missions.get(random.nextInt(missions.size()));
+            teamMissionRepository.save(
+                    TeamMission.builder()
+                            .season(season)
+                            .mission(randomMission.getMission())
+                            .team(team)
+                            .endedAt(endedAt)
+                            .build()
+            );
+        }
     }
 }
