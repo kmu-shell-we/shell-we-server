49ed39a0e72e6f171a9198c81885f4ae823c44fa
arirang030 <arirang030@kookmin.ac.kr>
2025-08-09 22:51:33 +0900

    [SW-92] refactor(schedule): 서비스 내 Collector 분리

--- src/main/java/com/github/kmu_shell_we/domain/user/_schedule/dto/response/ScheduleResponse.java
+++ src/main/java/com/github/kmu_shell_we/domain/user/_schedule/dto/response/ScheduleResponse.java
@@ -1,5 +1,6 @@
 package com.github.kmu_shell_we.domain.user._schedule.dto.response;
 
+import com.github.kmu_shell_we.domain.user._schedule.entity.Schedule;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,6 +8,7 @@ import lombok.Data;
 import java.time.DayOfWeek;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Data
 @AllArgsConstructor(staticName = "of")
@@ -14,4 +16,15 @@ import java.util.Map;
 public class ScheduleResponse {
 
     Map<DayOfWeek, List<TimeRange>> schedules;
+
+    public static ScheduleResponse from(List<Schedule> schedules) {
+
+        Map<DayOfWeek, List<TimeRange>> collect = schedules.stream()
+        .collect(Collectors.groupingBy(
+                Schedule::getDayOfWeek,
+                Collectors.mapping(TimeRange::from, Collectors.toList())
+        ));
+
+        return ScheduleResponse.of(collect);
+    }
 }
--- src/main/java/com/github/kmu_shell_we/domain/user/_schedule/service/ScheduleService.java
+++ src/main/java/com/github/kmu_shell_we/domain/user/_schedule/service/ScheduleService.java
@@ -3,7 +3,6 @@ package com.github.kmu_shell_we.domain.user._schedule.service;
 import com.fasterxml.jackson.databind.node.JsonNodeType;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.github.kmu_shell_we.domain.user._schedule.dto.response.ScheduleResponse;
-import com.github.kmu_shell_we.domain.user._schedule.dto.response.TimeRange;
 import com.github.kmu_shell_we.domain.user._schedule.entity.Schedule;
 import com.github.kmu_shell_we.domain.user._schedule.repository.ScheduleRepository;
 import com.github.kmu_shell_we.domain.user.entity.User;
@@ -15,11 +14,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
-import java.time.DayOfWeek;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Service
@@ -33,13 +29,9 @@ public class ScheduleService {
     @Transactional(readOnly = true)
     public ScheduleResponse getMySchedule(User user) {
 
-        Map<DayOfWeek, List<TimeRange>> collect = scheduleRepository.findAllByUser(user).stream()
-                .collect(Collectors.groupingBy(
-                        Schedule::getDayOfWeek,
-                        Collectors.mapping(TimeRange::from, Collectors.toList())
-                ));
+        List<Schedule> schedules = scheduleRepository.findAllByUser(user);
 
-        return ScheduleResponse.of(collect);
+        return ScheduleResponse.from(schedules);
     }
 
     @Transactional
@@ -55,7 +47,7 @@ public class ScheduleService {
                 .asBytes()
                 .getBody();
 
-        Map<DayOfWeek, List<TimeRange>> collect = new XmlMapper().readTree(response)
+        List<Schedule> schedules = new XmlMapper().readTree(response)
                 .get("table")
                 .get("subject")
                 .valueStream()
@@ -65,11 +57,8 @@ public class ScheduleService {
                 .flatMap(node -> node.getNodeType().equals(JsonNodeType.ARRAY) ? node.valueStream() : Stream.of(node))
                 .map(node -> Schedule.fromEverytime(node, user))
                 .peek(scheduleRepository::save)
-                .collect(Collectors.groupingBy(
-                        Schedule::getDayOfWeek,
-                        Collectors.mapping(TimeRange::from, Collectors.toList())
-                ));
+                .toList();
 
-        return ScheduleResponse.of(collect);
+        return ScheduleResponse.from(schedules);
     }
 }
