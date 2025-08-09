a5fa572d3c777b8aeeb1ee43d427b99e4c9b8d6c
arirang030 <arirang030@kookmin.ac.kr>
2025-08-09 23:14:11 +0900

    [SW-92] refactor(schedule): 팀 스케줄 조회 3차 구현(출력을 API와 동일한 형식으로 수정)

--- src/main/java/com/github/kmu_shell_we/domain/season/_team/_schedule/dto/response/UserSchedulePair.java
+++ src/main/java/com/github/kmu_shell_we/domain/season/_team/_schedule/dto/response/UserSchedulePair.java
@@ -1,6 +1,6 @@
 package com.github.kmu_shell_we.domain.season._team._schedule.dto.response;
 
-import com.github.kmu_shell_we.domain.user._schedule.dto.response.ScheduleResponse;
+import com.github.kmu_shell_we.domain.user._schedule.dto.response.TimeRange;
 import com.github.kmu_shell_we.domain.user._schedule.entity.Schedule;
 import com.github.kmu_shell_we.domain.user.dto.response.SimpleUserResponse;
 import com.github.kmu_shell_we.domain.user.entity.User;
@@ -8,7 +8,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.DayOfWeek;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Data
 @Schema(description = "개별 유저 시간표 응답 DTO")
@@ -16,13 +19,19 @@ import java.util.List;
 public class UserSchedulePair {
 
     private final SimpleUserResponse user;
-    private final ScheduleResponse schedule;
+    Map<DayOfWeek, List<TimeRange>> schedules;
 
     public static UserSchedulePair from(User user, List<Schedule> schedules) {
 
+        Map<DayOfWeek, List<TimeRange>> collect = schedules.stream()
+                .collect(Collectors.groupingBy(
+                        Schedule::getDayOfWeek,
+                        Collectors.mapping(TimeRange::from, Collectors.toList())
+                ));
+
         return UserSchedulePair.of(
                 SimpleUserResponse.from(user),
-                ScheduleResponse.from(schedules)
+                collect
         );
     }
 }
