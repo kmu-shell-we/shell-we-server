fc1c8a1098e4c751e8ba75d3cd5cd5eb6f208b5c
최비성 <qzsec299@naver.com>
2025-08-13 07:50:07 +0900

    [0.5.0] SW-96 순위를 조회할 수 있다. (#31)
    * [SW-96] feat(ranks repository): 랭킹 조회
    
    * [SW-96] feat(ranks service): 랭킹 조회
    
    * [SW-96] feat(ranks controller): 랭킹 조회

--- src/main/java/com/github/kmu_shell_we/domain/season/controller/SeasonController.java
+++ src/main/java/com/github/kmu_shell_we/domain/season/controller/SeasonController.java
@@ -1,15 +1,18 @@
 package com.github.kmu_shell_we.domain.season.controller;
 
+import com.github.kmu_shell_we.domain.season._team.dto.response.SimpleTeamResponse;
 import com.github.kmu_shell_we.domain.season.dto.response.SeasonResponse;
+import com.github.kmu_shell_we.domain.season.entity.Season;
 import com.github.kmu_shell_we.domain.season.service.SeasonService;
 import com.github.kmu_shell_we.global.response.ApiResponse;
 import com.github.kmu_shell_we.global.security.guard.MemberGuard;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @MemberGuard
 @RestController
@@ -26,4 +29,16 @@ public class SeasonController {
 
         return ApiResponse.ok(seasonService.getCurrentSeason());
     }
+
+
+    @GetMapping("/{season}/ranks")
+    @Operation(summary = "현재 시즌 랭크 조회")
+    public ApiResponse<List<SimpleTeamResponse>> getSeasonRanks(
+            @Parameter(description = "시즌 ID") @PathVariable Season season,
+            @Parameter(description = "상위 3개만 조회할지 여부") @RequestParam(required = false, defaultValue = "false") boolean simplified
+    ) {
+
+        return ApiResponse.ok(seasonService.getCurrentSeasonRanks(season, simplified));
+    }
+
 }
