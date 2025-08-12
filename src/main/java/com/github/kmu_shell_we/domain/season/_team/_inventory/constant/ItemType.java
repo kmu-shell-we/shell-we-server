26b8c723c438d3836edcda9c4196bebbb120db0f
BORB <qzsec299@naver.com>
2025-08-13 03:50:19 +0900

    [SW-94] feat(inventory entity): 인벤토리 조회 및 아이템 뽑기

---
+++ src/main/java/com/github/kmu_shell_we/domain/season/_team/_inventory/constant/ItemType.java
@@ -0,0 +1,8 @@
+package com.github.kmu_shell_we.domain.season._team._inventory.constant;
+
+public enum ItemType {
+
+    NEXT_MISSION_EXP_2X,
+    MISSION_RESET,
+    DECREASE_OTHER_TEAM_EXP,
+}
--- src/main/java/com/github/kmu_shell_we/domain/season/_team/entity/Team.java
+++ src/main/java/com/github/kmu_shell_we/domain/season/_team/entity/Team.java
@@ -1,5 +1,6 @@
 package com.github.kmu_shell_we.domain.season._team.entity;
 
+import com.github.kmu_shell_we.domain.season._team._inventory.entity.Inventory;
 import com.github.kmu_shell_we.domain.season._team._user_team.entity.UserTeam;
 import com.github.kmu_shell_we.domain.season.entity.Season;
 import com.github.kmu_shell_we.global.infra.mysql.BaseSchema;
@@ -35,4 +36,7 @@ public class Team extends BaseSchema {
     @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @Builder.Default
     List<UserTeam> userTeams = List.of();
+
+    @OneToOne(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
+    Inventory inventory;
 }
