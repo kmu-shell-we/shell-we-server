ba5be162671cfecd699d8604f9ab9903816ac07b
BORB <qzsec299@naver.com>
2025-08-13 13:42:14 +0900

    [SW-94] refactor(item): Team -> Item 다대일 구조로 변경

--- src/main/java/com/github/kmu_shell_we/domain/season/_team/entity/Team.java
+++ src/main/java/com/github/kmu_shell_we/domain/season/_team/entity/Team.java
@@ -1,6 +1,6 @@
 package com.github.kmu_shell_we.domain.season._team.entity;
 
-import com.github.kmu_shell_we.domain.season._team._mission_team.entity.TeamMission;
+import com.github.kmu_shell_we.domain.season._team._item.entity.Item;
 import com.github.kmu_shell_we.domain.season._team._user_team.entity.UserTeam;
 import com.github.kmu_shell_we.domain.season.entity.Season;
 import com.github.kmu_shell_we.global.infra.mysql.BaseSchema;
@@ -37,6 +37,6 @@ public class Team extends BaseSchema {
     @Builder.Default
     List<UserTeam> userTeams = List.of();
 
-    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-    List<TeamMission> teamMissions;
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Item> items = List.of();
 }
