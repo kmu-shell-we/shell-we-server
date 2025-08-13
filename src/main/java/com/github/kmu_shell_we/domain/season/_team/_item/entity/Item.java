780367e78834fc70bd47522807f6329b3f9485da
BORB <qzsec299@naver.com>
2025-08-13 13:42:14 +0900

    [SW-94] refactor(item): Team -> Item 다대일 구조로 변경

---
+++ src/main/java/com/github/kmu_shell_we/domain/season/_team/_item/entity/Item.java
@@ -0,0 +1,25 @@
+package com.github.kmu_shell_we.domain.season._team._item.entity;
+
+import com.github.kmu_shell_we.domain.season._team._item.constant.ItemType;
+import com.github.kmu_shell_we.domain.season._team.entity.Team;
+import com.github.kmu_shell_we.global.infra.mysql.BaseSchema;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder(toBuilder = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item extends BaseSchema {
+
+    @ManyToOne
+    @JoinColumn(name = "team_id", nullable = false)
+    Team team;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    ItemType type;
+}
--- src/main/java/com/github/kmu_shell_we/domain/season/_team/entity/Team.java
+++ src/main/java/com/github/kmu_shell_we/domain/season/_team/entity/Team.java
@@ -1,6 +1,6 @@
 package com.github.kmu_shell_we.domain.season._team.entity;
 
-import com.github.kmu_shell_we.domain.season._team._inventory.entity.Inventory;
+import com.github.kmu_shell_we.domain.season._team._item.entity.Item;
 import com.github.kmu_shell_we.domain.season._team._user_team.entity.UserTeam;
 import com.github.kmu_shell_we.domain.season.entity.Season;
 import com.github.kmu_shell_we.global.infra.mysql.BaseSchema;
@@ -37,6 +37,6 @@ public class Team extends BaseSchema {
     @Builder.Default
     List<UserTeam> userTeams = List.of();
 
-    @OneToOne(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
-    Inventory inventory;
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Item> items = List.of();
 }
