d43fd040b1dd7db423098187e9b1887c7c04c24e
arirang030 <arirang030@kookmin.ac.kr>
2025-09-06 19:27:39 +0900

    [SW-124] chore(item): 예외 코드 수정

--- src/main/java/com/github/kmu_shell_we/domain/season/_team/_item/exception/ItemExceptions.java
+++ src/main/java/com/github/kmu_shell_we/domain/season/_team/_item/exception/ItemExceptions.java
@@ -9,7 +9,7 @@ import lombok.RequiredArgsConstructor;
 public enum ItemExceptions implements ApiExceptionCode {
 
     FAILED_TO_DRAW("ITEM_001", "아이템을 뽑는데 실패했습니다."),
-    NOT_ENOUGH_POINT("ITEM_001", "포인트가 부족합니다."),
+    NOT_ENOUGH_POINT("ITEM_002", "포인트가 부족합니다."),
     ;
 
     private final String code;
